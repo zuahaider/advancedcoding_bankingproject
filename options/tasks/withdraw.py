d5c63e806ebfe356fd817821193918126306418b
Zuha Haider <zuha.haider110@gmail.com>
2024-06-15 13:35:05 +0200

    changes added to withdraw

--- options/tasks/withdraw.py
+++ options/tasks/withdraw.py
@@ -1,22 +1,22 @@
 import sqlite3
 from options.tasks.account_action import AccountAction
 
-# Withdraw is a child class that inherits from the parent class AccountAction.
+# child class: Withdraw inherits from the parent class: AccountAction
 class Withdraw(AccountAction):
     def __init__(self, account_no, amount):
-        # Initializing the parent class with account number
+        # initializing the parent class with account number
         super().__init__(account_no)
         self.amount = amount
 
     def execute(self):
-        # Implementations for withdrawing money
+        # specific implementations for withdrawing money
         conn = sqlite3.connect('banking_management.db')
         cursor = conn.cursor()
         cursor.execute('SELECT balance FROM accounts WHERE account_no = ?', (self.account_no,))
         balance = cursor.fetchone()[0]
         
         if balance >= self.amount:
-            # Update the balance in the database
+            # update the balance in the database
             new_balance = balance - self.amount
             cursor.execute('UPDATE accounts SET balance = ? WHERE account_no = ?', (new_balance, self.account_no))
             conn.commit()
