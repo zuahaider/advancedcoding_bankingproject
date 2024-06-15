95b7ab23d83b6e7adf7522b43d5f60e9cc04214d
Zuha Haider <zuha.haider110@gmail.com>
2024-06-15 13:25:46 +0200

    changes added to show_acc_details

--- options/show_info/show_acc_details.py
+++ options/show_info/show_acc_details.py
@@ -1,12 +1,12 @@
 from options.tasks.account_action import AccountAction
 import sqlite3
 
-# ShowAccountDetails is a child class that inherits from the parent class AccountAction.
+# childclass: ShowAccountDetails inherits from the parent class: AccountAction
 class ShowAccountDetails(AccountAction):
     def execute(self):
-        # Specific implementation for showing account details.
+        # specific implementation for showing account details.
         account = self.get_account()
         if account:
-            print(f"Holder Name: {account[3]}, Account No: {account[0]}, Password: {account[1]}, Balance: {account[2]}")
+            print(f"Holder Name: {account[3]}, Account No: {account[0]}, Password: {account[1]}, Balance: {account[2]}") # using f string to format the string
         else:
             print("Account not found.")
