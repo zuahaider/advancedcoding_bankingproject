daf4729d8e7b17d341e3446f69f631f1fba0d454
Zuha Haider <zuha.haider110@gmail.com>
2024-06-15 13:33:07 +0200

    added changes to deposit

--- options/tasks/deposit.py
+++ options/tasks/deposit.py
@@ -1,24 +1,24 @@
 import sqlite3
 from options.tasks.account_action import AccountAction
 
-# Deposit is a child class that inherits from the parent class AccountAction.
+# child class: Deposit inherits from the parent class: AccountAction
 class Deposit(AccountAction):
     def __init__(self, account_no, amount):
-        # Initialize the parent class with account number.
+        # initializing the parent class with account number
         super().__init__(account_no)
         self.amount = amount
 
     def execute(self):
-        # Implementations for depositing money
+        # specific implementations for depositing money
         conn = sqlite3.connect('banking_management.db')
         cursor = conn.cursor()
         cursor.execute('UPDATE accounts SET balance = balance + ? WHERE account_no = ?', (self.amount, self.account_no))
         conn.commit()
         
-        # Fetch the updated balance from the database
+        # fetch the updated balance from the database
         cursor.execute('SELECT balance FROM accounts WHERE account_no = ?', (self.account_no,))
-        updated_balance = cursor.fetchone()[0]
+        updated_balance = cursor.fetchone()[0] 
         
         conn.close()
         print("Deposit successful!")
-        print("Your updated account balance is:", updated_balance)  # Print the updated balance
+        print("Your updated account balance is:", updated_balance)  # print the updated balance
